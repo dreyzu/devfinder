a61728239664241f9e8854435008d9ac7d49b320
dreyzu <tristan.derez@gmail.com>
2021-11-30 14:59:29 +0100

    add children={undefined} to component App

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <App children={undefined} />
     </React.StrictMode>,
     document.getElementById("root")
 );
