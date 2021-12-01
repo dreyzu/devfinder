e3018789fa5b1f7b5bb0297ed504e81046e2c102
dreyzu <tristan.derez@gmail.com>
2021-12-01 13:06:40 +0100

    add colorModeValue

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 import "@fontsource/space-mono";
 
-import { Box, Flex, Heading, Image, Link, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Link, Text, useColorModeValue } from "@chakra-ui/react";
 import { useRemoteData } from "react-hook-remotedata";
 
 export const Card = () => {
@@ -10,13 +10,20 @@ export const Card = () => {
         <Flex direction="row" w="230px" h="70px" mt="32px" fontFamily="space mono" justifyContent="flex-start">
             <Image borderRadius="full" boxSize="70px" src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
             <Flex direction="column" ml="20px" justifyContent="center">
-                <Heading fontSize="16px" fontWeight="bold" fontFamily="space mono">
+                <Heading
+                    fontSize="16px"
+                    fontWeight="bold"
+                    fontFamily="space mono"
+                    color={useColorModeValue("grey-black", "white")}
+                >
                     Name
                 </Heading>
                 <Link color="blue" fontSize="13px">
                     @pseudo
                 </Link>
-                <Text fontSize="13px">Joined Date</Text>
+                <Text fontSize="13px" color={useColorModeValue("grey-blue", "white")}>
+                    Joined Date
+                </Text>
             </Flex>
         </Flex>
     );
