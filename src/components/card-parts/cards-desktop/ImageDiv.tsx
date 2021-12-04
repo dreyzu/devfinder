4bc2bf8c66852d8c4a9ea161395abafc1943143d
dreyzu <tristan.derez@gmail.com>
2021-12-05 00:01:02 +0100

    remove margin top

--- src/components/card-parts/cards-desktop/ImageDiv.tsx
+++ src/components/card-parts/cards-desktop/ImageDiv.tsx
@@ -3,7 +3,7 @@ import { Flex, Image } from "@chakra-ui/react";
 //desktop only
 export const ImageDiv = () => {
     return (
-        <Flex direction="column" mt="48px" display={["none", "none", "none", "flex"]} ml="48px" h="100%" w="117px">
+        <Flex direction="column" display={["none", "none", "none", "flex"]} ml="48px" h="100%" w="117px">
             <Image borderRadius="full" boxSize={["70px", "117px"]} src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
         </Flex>
     );
