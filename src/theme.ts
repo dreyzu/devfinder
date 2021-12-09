a021871879197063285180d68edbdc39b44367d1
dreyzu <tristan.derez@gmail.com>
2021-12-09 20:57:25 +0100

    change extension

---
+++ src/theme.ts
@@ -0,0 +1,31 @@
+import "@fontsource/space-mono";
+
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
+
+//need to change this
+const breakpoints = createBreakpoints({
+    sm: "37.5em",
+    md: "76.8em",
+    lg: "144em",
+    xl: "256em",
+});
+
+export const theme = extendTheme({
+    breakpoints,
+    config: {
+        initialColorMode:
+            window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light",
+        useSystemColorMode: true,
+    },
+    colors: {
+        blue: "#0079FF",
+        white: "#FEFEFE",
+        "grey-blue": "#697C9A",
+        "pale-blue": "#4B6A9B",
+        "grey-black": "#2B3442",
+        "grey-white": "#F6F8FF",
+        "marine-blue": "#141D2F",
+        "blue-high": "#1E2A47",
+    },
+});
