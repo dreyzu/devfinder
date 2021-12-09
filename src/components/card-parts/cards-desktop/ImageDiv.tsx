91f9826cabc08c011a0cafdb46fe3ef272b53896
dreyzu <tristan.derez@gmail.com>
2021-12-10 00:30:27 +0100

    change value of boxSize

--- src/components/card-parts/cards-desktop/ImageDiv.tsx
+++ src/components/card-parts/cards-desktop/ImageDiv.tsx
@@ -1,11 +1,11 @@
 import { Flex, Image } from "@chakra-ui/react";
 
 //desktop only
-// conversion to em or rem is done but still need to check on figma for real sizes
+
 export const ImageDiv = () => {
     return (
         <Flex direction="column" display={["none", "none", "none", "flex"]} ml="4.8em" h="100%" w="11.7em">
-            <Image borderRadius="full" boxSize={["7em", "11.7em"]} src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+            <Image borderRadius="full" boxSize="11.7em" src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
         </Flex>
     );
 };
