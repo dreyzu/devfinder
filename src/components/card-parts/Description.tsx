1c638d66170e4d173abae28039e43d7206f1141b
dreyzu <tristan.derez@gmail.com>
2021-12-10 20:04:07 +0100

    add a breakpoint for font-size

--- src/components/card-parts/Description.tsx
+++ src/components/card-parts/Description.tsx
@@ -7,7 +7,7 @@ export const Description = () => {
             h="75px"
             w="100%"
             alignItems="center"
-            fontSize={["1.2rem", "1.5rem"]}
+            fontSize={["1.2rem", "1.3rem", "1.5rem"]}
         >
             <Text lineHeight="25px" color={useColorModeValue("pale-blue", "white")}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec odio. Quisque volutpat mattis eros.
