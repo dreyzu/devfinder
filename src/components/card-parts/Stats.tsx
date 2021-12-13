922c3286b7d47518fe3cbcb2c214eb43aec1f932
dreyzu <tristan.derez@gmail.com>
2021-12-13 20:03:27 +0100

    change layout of div

--- src/components/card-parts/Stats.tsx
+++ src/components/card-parts/Stats.tsx
@@ -6,38 +6,38 @@ export const Stats = () => {
         <Flex
             borderRadius="1em"
             background={useColorModeValue("grey-white", "marine-blue")}
-            justifyContent="space-around"
+            justifyContent={["space-between", "space-between", "initial"]}
             alignItems="center"
             mt="23px"
+            px={["15px", "15px", 0]}
             h="85px"
             w="100%"
-            px="15px"
             fontSize={["1.1rem", "1.1rem", "1.3rem", "null"]}
         >
-            <Flex direction="column" alignItems={["center", "flex-start", "Flex-start"]}>
+            <Flex direction="column" pl={["0px", "0px", "32px"]} alignItems={["center", "center", "flex-start"]}>
                 <Text color={useColorModeValue("pale-blue", "white")}>Repos</Text>
                 <Text
-                    fontSize={["1.2rem", "2.2rem"]}
+                    fontSize={["1.6rem", "1.6rem", "2.2rem"]}
                     fontWeight="bold"
                     color={useColorModeValue("grey-black", "white")}
                 >
                     8
                 </Text>
             </Flex>
-            <Flex direction="column" alignItems={["center", "flex-start", "Flex-start"]}>
+            <Flex direction="column" pl={[0, 0, "99px"]} alignItems={["center", "center", "flex-start"]}>
                 <Text color={useColorModeValue("pale-blue", "white")}>Followers</Text>
                 <Text
-                    fontSize={["1.2rem", "2.2rem"]}
+                    fontSize={["1.6rem", "1.6rem", "2.2rem"]}
                     fontWeight="bold"
                     color={useColorModeValue("grey-black", "white")}
                 >
                     3938
                 </Text>
             </Flex>
-            <Flex direction="column" alignItems={["center", "flex-start", "Flex-start"]}>
+            <Flex direction="column" pl={[0, 0, "82px"]} alignItems={["center", "center", "flex-start"]}>
                 <Text color={useColorModeValue("pale-blue", "white")}>Following</Text>
                 <Text
-                    fontSize={["1.2rem", "2.2rem"]}
+                    fontSize={["1.6rem", "1.6rem", "2.2rem"]}
                     fontWeight="bold"
                     color={useColorModeValue("grey-black", "white")}
                 >
