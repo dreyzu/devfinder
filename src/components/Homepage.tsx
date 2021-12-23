a066d0de7d87ff0341d81ef61b3ba4063faf6eb7
dreyzu <tristan.derez@gmail.com>
2021-12-23 14:01:50 +0100

    add context for data

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,21 +1,27 @@
 import { Flex, useColorModeValue } from "@chakra-ui/react";
+import { createContext, useState } from "react";
 
+import { DataContext } from "./DataContext";
 import { Header } from "./Header";
 import { Profile } from "./Profile";
 import { SearchBar } from "./SearchBar";
 
 export const HomePage = () => {
+    const [data, setData] = useState(null);
+    console.log(data);
     return (
-        <Flex
-            direction="column"
-            alignItems="center"
-            bg={useColorModeValue("grey-white", "marine-blue")}
-            w="100%"
-            minHeight="100%"
-        >
-            <Header />
-            <SearchBar />
-            <Profile />
-        </Flex>
+        <DataContext.Provider value={{ data, setData }}>
+            <Flex
+                direction="column"
+                alignItems="center"
+                bg={useColorModeValue("grey-white", "marine-blue")}
+                w="100%"
+                minHeight="100%"
+            >
+                <Header />
+                <SearchBar />
+                <Profile />
+            </Flex>
+        </DataContext.Provider>
     );
 };
