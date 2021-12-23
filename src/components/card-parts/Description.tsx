435a28e1b98d434d6f479fb0ec4de4de705b2096
dreyzu <tristan.derez@gmail.com>
2021-12-23 14:03:52 +0100

    add data

--- src/components/card-parts/Description.tsx
+++ src/components/card-parts/Description.tsx
@@ -1,6 +1,10 @@
 import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import { useContext } from "react";
+
+import { DataContext } from "../DataContext";
 
 export const Description = () => {
+    const { data, setData } = useContext(DataContext);
     return (
         <Flex
             mt={["33px", "33px", "24px", "20px"]}
@@ -8,9 +12,10 @@ export const Description = () => {
             w="100%"
             alignItems="center"
             fontSize={["1.2rem", "1.3rem", "1.5rem"]}
+            opacity={data?.bio ? "100%" : "50%"}
         >
             <Text lineHeight="25px" color={useColorModeValue("pale-blue", "white")}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec odio. Quisque volutpat mattis eros.
+                {data?.bio ? data.bio : "This user has no bio"}
             </Text>
         </Flex>
     );
