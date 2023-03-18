0a59dd37049fd8a09d6ecb6c3352e5520c375d76
Tristan Derez <Tristan.derez@gmail.com>
2023-03-18 10:39:28 +0100

    feat: create defaultData file

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,13 +1,15 @@
 import { Flex, useColorModeValue } from "@chakra-ui/react";
 import { useState } from "react";
 
+import { defaultData } from "@/data/defaultData";
+
 import { DataContext } from "./DataContext";
 import { Header } from "./Header";
 import { Profile } from "./Profile";
 import { SearchBar } from "./SearchBar";
 
 export const HomePage = () => {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState(defaultData);
 
     return (
         <DataContext.Provider value={{ data, setData }}>
